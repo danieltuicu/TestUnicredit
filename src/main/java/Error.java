142a4aa61c5e9881fbae2f32a329d79918bab6e2
danieltuicu <daniel.tuicu@yahoo.com>
2017-05-30 00:17:45 +0300

    Add files via upload

---
+++ src/main/java/Error.java
@@ -0,0 +1,6 @@
+public class Error extends Response{
+
+    public Error(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/Response.java
@@ -0,0 +1,12 @@
+public class Response {
+
+    protected String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
